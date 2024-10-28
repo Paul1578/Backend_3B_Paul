45df960a01262a293897adf2cfe7499485d45388
unknown <lpa.carrillo@yavirac.edu.ec>
2024-10-28 10:57:53 -0500

    No Funca

---
+++ docs/auth-jwt-Passport.md
@@ -0,0 +1,19 @@
+# Instalación
+```
+npm i --save @nestjs/jwt passport-jwt bcrypt
+npm i --save-dev @types/passport-jwt
+```
+#Arracanmos el proyecto
+```
+npm run start:dev
+```
+# Modules & Resurce
+```
+nest g mo modules/auth
+nest res modules/users
+```
+# Controllers & Services
+```
+nest g co modules/auth
+nest g s modules/auth
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthController } from './modules/auth/auth.controller';
+import { AuthService } from './modules/auth/auth.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
