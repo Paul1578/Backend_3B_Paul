11598cb8eb1a01de99b19481447fd992a9306b74
Josselyn Isabel Toasa Tutalcha <lpa.carrillo@yavirac.edu.ec>
2024-11-10 21:17:02 -0500

    commit class 8

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterAuthDto } from './dto/register-auth.dto';
 import { LoginAuthDto } from './dto/login-auth.dto';
-import { AuthService } from './authService';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
     constructor(private jwService: JwtService){}
     login(credenciales: LoginAuthDto){
-
+        
+        let paylod={email:"admin@gmail.com", id:1}
+        const token= this.jwService.sign(paylod)
+        return {token:token};
     }
 }
